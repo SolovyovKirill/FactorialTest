51cb0786b5fc739ea59268245d68518f5f40a025
Salauyou Kiryl <Solowyow.kirya@gmail.com>
2022-10-10 12:28:18 +0300

    stateful view model

--- app/src/main/java/com/example/factorialtest/MainActivity.kt
+++ app/src/main/java/com/example/factorialtest/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.factorialtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.factorialtest.databinding.ActivityMainBinding
 
@@ -28,26 +27,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun observeViewModel() {
-        viewModel.progress.observe(this) {
-            if (it) {
-                binding.progressBarLoading.visibility = View.VISIBLE
-                binding.buttonCalculate.isEnabled = false
-            } else {
-                binding.progressBarLoading.visibility = View.GONE
-                binding.buttonCalculate.isEnabled = true
-            }
-        }
-        viewModel.error.observe(this) {
-            if (it) {
+        viewModel.state.observe(this) {
+            if (it.isError) {
                 Toast.makeText(
                     this,
                     "You did nit entered value",
                     Toast.LENGTH_SHORT
                 ).show()
             }
-        }
-        viewModel.factorial.observe(this) {
-            binding.textViewFactorial.text = it
+            if (it.isInProgress) {
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+            binding.textViewFactorial.text = it.factorial
         }
     }
 }
---
+++ app/src/main/java/com/example/factorialtest/State.kt
@@ -0,0 +1,7 @@
+package com.example.factorialtest
+
+class State(
+    val isError:Boolean = false,
+    val isInProgress: Boolean = false,
+    val factorial: String = ""
+)
